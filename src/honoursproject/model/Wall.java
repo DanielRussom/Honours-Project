3285dae15196fe0127ae69c2a227a21e7aa2ea9d
Daniel Russom <daniel.russom00@gmail.com>
2018-01-19 18:59:00 +0000

    Wall Class
    -Wall class added

---
+++ src/honoursproject/model/Wall.java
@@ -0,0 +1,16 @@
+package honoursproject.model;
+
+import javafx.scene.Node;
+
+public class Wall extends Element {
+
+	public Wall() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Wall(Node image) {
+		super(image);
+		// TODO Auto-generated constructor stub
+	}
+
+}
