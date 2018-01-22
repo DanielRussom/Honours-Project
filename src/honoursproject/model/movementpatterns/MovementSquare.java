4abff08b33092f243eccf0bbbf61ac411d0bce6d
Daniel Russom <daniel.russom00@gmail.com>
2018-01-22 14:50:03 +0000

    Square Comments
    -Commented the square movement pattern

--- src/honoursproject/model/movementpatterns/MovementSquare.java
+++ src/honoursproject/model/movementpatterns/MovementSquare.java
@@ -7,24 +7,36 @@ public class MovementSquare implements Movable {
 
 	@Override
 	public void move(Player player) {
-		if(player.getYVel()<0) {
-			if(player.getYPosition() < 30) {
+		// Checks if the element is moving up
+		if (player.getYVel() < 0) {
+			// Checks if the element has reached the top
+			if (player.getYPosition() < 30) {
+				// Moves the element left
 				player.setYVel(0);
 				player.setXVel(-2);
 			}
-		} else if(player.getYVel()>0) {
-			if(player.getBottomSide() > player.getImage().getParent().getLayoutBounds().getHeight()-30) {
+			// Checks if the element is moving down
+		} else if (player.getYVel() > 0) {
+			// Checks if the element has reached the bottom
+			if (player.getBottomSide() > player.getImage().getParent().getLayoutBounds().getHeight() - 30) {
+				// Moves the element right
 				player.setYVel(0);
 				player.setXVel(2);
 			}
 		}
-		if(player.getXVel()<0) {
-			if(player.getXPosition() < 30) {
+		// Checks if the element is moving left
+		if (player.getXVel() < 0) {
+			// Checks if the element has reached the left edge
+			if (player.getXPosition() < 30) {
+				// Moves the element down
 				player.setYVel(2);
 				player.setXVel(0);
 			}
-		} else if(player.getXVel()>0) {
-			if(player.getRightSide() > player.getImage().getParent().getLayoutBounds().getWidth()-30) {
+			// Checks if the element is moving right
+		} else if (player.getXVel() > 0) {
+			// Checks if the element has reached the right edge
+			if (player.getRightSide() > player.getImage().getParent().getLayoutBounds().getWidth() - 30) {
+				// Move the element up
 				player.setYVel(-2);
 				player.setXVel(0);
 			}
@@ -36,9 +48,9 @@ public class MovementSquare implements Movable {
 		player.setXVel(0);
 		player.setYVel(2);
 	}
-	
-public void moveSetUp(Player player, double maxNoise) {
-		
+
+	public void moveSetUp(Player player, double maxNoise) {
+
 	}
 
 }
