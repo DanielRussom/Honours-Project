744536b0b70f775701db064e1ad1a251a6233484
Daniel Russom <daniel.russom00@gmail.com>
2018-01-27 21:58:35 +0000

    Line Shooting
    -Added shooting to Line movement pattern based on the target's position

--- src/honoursproject/model/movementpatterns/MovementLine.java
+++ src/honoursproject/model/movementpatterns/MovementLine.java
@@ -1,5 +1,6 @@
 package honoursproject.model.movementpatterns;
 
+import honoursproject.GameController;
 import honoursproject.model.Movable;
 import honoursproject.model.Player;
 import honoursproject.util.MovementNoiseApplier;
@@ -8,6 +9,7 @@ public class MovementLine implements Movable {
 	private double maxOffset = 5;
 	private double currentXOffset = 0;
 	private double currentYOffset = 0;
+	private int shootingType = 1;
 
 	@Override
 	public void move(Player player) {
@@ -22,6 +24,24 @@ public class MovementLine implements Movable {
 			// If at the right edge, move left
 			if (player.getRightSide() > player.getImage().getParent().getLayoutBounds().getWidth() - 30) {
 				player.setXVel(-2);
+			}
+		} // Checks if this element is able to shoot
+		if (shootingType == 1) {
+			Player target = GameController.getCurrentPlayer();
+			double yDistance = player.getVerticalCenter(0) - target.getVerticalCenter(0);
+			double xDistance = player.getHorizontalCenter(0) - target.getHorizontalCenter(0);
+			if (Math.abs(yDistance) > Math.abs(xDistance)) {
+				if (yDistance > 0) {
+					player.shoot('U');
+				} else {
+					player.shoot('D');
+				}
+			} else {
+				if (xDistance > 0) {
+					player.shoot('L');
+				} else {
+					player.shoot('R');
+				}
 			}
 		}
 		// Checks if there is noise to be applied
@@ -45,14 +65,17 @@ public class MovementLine implements Movable {
 
 	@Override
 	public void moveSetUp(Player player, int shootingType) {
-		// TODO Auto-generated method stub
-		
+		player.setXVel(2);
+		player.setYVel(0);
+		this.shootingType = shootingType;
 	}
 
 	@Override
 	public void moveSetUp(Player player, double maxNoise, int shootingType) {
-		// TODO Auto-generated method stub
-		
+		player.setXVel(2);
+		player.setYVel(0);
+		this.shootingType = shootingType;
+		maxOffset = maxNoise;
 	}
 
 }
